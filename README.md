17ec76fefa70004dad15890928575263005d05fd
gpsgui <gp_simao@hotmail.com>
2020-04-10 21:51:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+=========================
+An easter egg scrapper
+It's 2020's Easter and the FilipeFlop hardware store is giving %50 on many itens, but this discount is hidden, like an easter egg, on unknown products.
+Here is a python scrapper that do the job of scanning the entire store and returning the easter eggs URLs.
---
+++ scrapper.py
@@ -0,0 +1,53 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+url_categories = [
+    # "https://www.filipeflop.com/categoria/arduino/",
+    # "https://www.filipeflop.com/categoria/campanha_natal_filipeflop/",
+    # "https://www.filipeflop.com/categoria/componentes-eletronicos/",
+    # "https://www.filipeflop.com/categoria/display-e-iluminacao/",
+    # "https://www.filipeflop.com/categoria/impressao-3d/",
+    # "https://www.filipeflop.com/categoria/livros/",
+    # "https://www.filipeflop.com/categoria/modulos/",
+    # "https://www.filipeflop.com/categoria/outlet/",
+    # "https://www.filipeflop.com/categoria/partes/",
+    "https://www.filipeflop.com/categoria/placas-de-desenvolvimento/"
+    # "https://www.filipeflop.com/categoria/prototipagem/",
+    # "https://www.filipeflop.com/categoria/raspberry-pi/",
+    # "https://www.filipeflop.com/categoria/robotica/",
+    # "https://www.filipeflop.com/categoria/sensores/",
+    # "https://www.filipeflop.com/categoria/teste-e-medicao/",
+    # "https://www.filipeflop.com/categoria/wearable/",
+    # "https://www.filipeflop.com/categoria/wireless-e-iot/"
+]
+url_regulamento = "https://www.filipeflop.com/regulamento/"
+
+start_time = time.time()
+
+eggs_urls = []
+for category_url in url_categories:
+    data = {'ppp': -1}      # Products Per Page, -1 means no limits
+    category_page = requests.get(category_url, data)
+    soup = BeautifulSoup(category_page.content, 'html.parser')
+    itens = soup.find_all('div', class_='product-loop-header product-item__header')
+    for i in itens:
+        prod_url = i.contents[1].attrs['href']
+        prod_page = requests.get(prod_url)
+        soup_prod = BeautifulSoup(prod_page.content, 'html.parser')
+        div_prod_summary = soup_prod.find_all('div',class_='summary entry-summary')
+        # looking for this URL, that means an easter egg
+        if len(div_prod_summary[0].findAll(href = url_regulamento)) > 0: 
+            print(prod_url)
+            eggs_urls.append(prod_url + "\n")
+
+u_eggs_urls = set(eggs_urls)      # converting to set and removind duplicates
+
+with open("eggs_filipeflop.txt", "w") as f:
+    f.writelines(u_eggs_urls)
+    end_time = time.time()
+    f.write("\n Elapsed time : " + str(end_time - start_time) + " seconds")
+
+end_time = time.time()
+
+print("Elapsed time : " + str(end_time - start_time) + " seconds")
